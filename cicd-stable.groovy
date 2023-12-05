4fe719aeea1b7bbe90dc41b41ea3a171c7bf320c
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2023-12-04 22:16:43 -0500

    Update cicd-stable.groovy

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -12,7 +12,7 @@ node('linux')
   }
 
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/thesilversearcherport.git'), string(name: 'PORT_DESCRIPTION', value: 'A code-searching tool similar to ack, but faster.', string(name: 'BUILD_LINE', value: 'STABLE')]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/thesilversearcherport.git'), string(name: 'PORT_DESCRIPTION', value: 'A code-searching tool similar to ack, but faster.'), string(name: 'BUILD_LINE', value: 'STABLE')]
   }
 }
 
